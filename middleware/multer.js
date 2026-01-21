cdd92690cab6f5704f6dd49b0b46db42286147bd
Sameer Alkassar <samir.alkcar@gmail.com>
2026-01-21 19:13:26 +0200

    fixing: multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,13 +1,9 @@
-import multer from "multer";
+import multer from 'multer';
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+}});
 
-const upload = multer({
-  storage,
-  limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
-  },
-});
-
+const upload = multer({ storage: storage });
 export default upload;
-
