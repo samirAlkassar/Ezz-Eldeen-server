567f7461aa98bf93d82301feec4e20b1aa85dfb7
Sameer Alkassar <samir.alkcar@gmail.com>
2026-01-21 19:10:33 +0200

    fixing multer

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,22 +1,13 @@
 import multer from "multer";
-import fs from "fs";
-import path from "path";
 
-// Create uploads folder automatically if missing
-const uploadPath = path.join(process.cwd(), "uploads");
-if (!fs.existsSync(uploadPath)) {
-  fs.mkdirSync(uploadPath);
-}
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadPath);
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, // 5MB
   },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  }
 });
 
-const upload = multer({ storage });
-
 export default upload;
+
